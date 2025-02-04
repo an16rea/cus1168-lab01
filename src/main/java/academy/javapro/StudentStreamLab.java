3083c36983ed1e76924561b51a941309bbef34bd
an16rea <andrea.dass16@gmail.com>
2025-02-03 23:23:31 -0500

    Update StudentStreamLab.java

--- src/main/java/academy/javapro/StudentStreamLab.java
+++ src/main/java/academy/javapro/StudentStreamLab.java
@@ -1,7 +1,8 @@
 package academy.javapro;
 
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StudentStreamLab {
     public static void main(String[] args) {
@@ -15,11 +16,36 @@ public class StudentStreamLab {
         );
 
         // TODO - Filtering: Students with GPA > 3.0
+        List<Student> highGpaStudents = students.stream()
+            .filter(s -> s.getGpa() > 3.0)
+            .collect(Collectors.toList());
+
+            System.out.println("Students with GPA > 3.0: " + highGpaStudents);
+
 
         // TODO - Mapping: Extract names of students with GPA > 3.0
+        List<String> highGpaStudentNames = students.stream()
+            .filter(s -> s.getGpa() > 3.0)
+            .map(Student::getName)
+            .collect(Collectors.toList());
+
+            System.out.println ("High GPA student names: " + highGpaStudents);
+        
 
         // TODO - Reducing: Calculate the average GPA of all students
+        double averageGpa = students.stream()
+                .mapToDouble(Student::getGpa)
+                .average()
+                .orElse(0.0);
+
+        System.out.println ("Average GPA: " + averageGpa);
 
         // TODO Collecting: Collect all "Junior" students into a list
+        List<Student> juniorStudents = students.stream()
+                .filter(s -> s.getCollegeYear().equals("Junior"))
+                .collect(Collectors.toList());
+
+        System.out.println("List of Junior students: " + juniorStudents);
+
     }
 }
